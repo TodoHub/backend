0a8fb6a6d15fb1914b20dd1a03b6a8f75984361a
Marcio Gabriel <gabriel09@gmail.com>
2024-03-14 03:03:31 -0300

    Start Project

---
+++ todohub/src/main/java/com/hiro/todohub/TodohubApplication.java
@@ -0,0 +1,13 @@
+package com.hiro.todohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodohubApplication.class, args);
+	}
+
+}
---
+++ todohub/src/test/java/com/hiro/todohub/TodohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiro.todohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
